f0cde11aa7ecd3a5c18bb52783a4c97dd2445323
Serena Sensini <serena.sensini@gmail.com>
2020-01-30 14:06:09 +0100

    fix

--- train_chatbot.py
+++ train_chatbot.py
@@ -8,12 +8,11 @@ import pickle
 
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout
 from keras.optimizers import SGD
-import random
 
 lemmatizer = WordNetLemmatizer()
-words=[]
+words = []
 classes = []
 documents = []
 ignore_words = ['?', '!']
@@ -21,14 +20,15 @@ data_file = open('intents.json').read()
 intents = json.loads(data_file)
 print(intents)
 
-
+# intents: gruppi di conversazioni-tipo
+# patterns: possibili interazioni dell'utente
 for intent in intents['intents']:
     for pattern in intent['patterns']:
 
-        # take each word and tokenize it
+        # tokenizzo ogni parola
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        # adding documents
+        # aggiungo all'array documents
         documents.append((w, intent['tag']))
 
         # adding classes to our class list
@@ -36,51 +36,35 @@ for intent in intents['intents']:
             classes.append(intent['tag'])
 
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-#words = sorted(list(set(words)))
 
-#classes = sorted(list(set(classes)))
+pickle.dump(words, open('words.pkl','wb'))
+pickle.dump(classes, open('classes.pkl','wb'))
 
-print (len(documents), "documents")
-print("###########DOCUMENTS")
-print(documents)
-print (len(classes), "classes", classes)
-
-print (len(words), "unique lemmatized words", words)
-
-
-pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
-
-# initializing training data
+# preparazione per l'addestramento della rete
 training = []
 output_empty = [0] * len(classes)
 for doc in documents:
-    # initializing bag of words
+    # bag of words
     bag = []
-    # list of tokenized words for the pattern
+    # lista di tokens
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
+    # lemmatizzazione dei token
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    # se la parola matcha, inserisco 1, altriment 0
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
 
     training.append([bag, output_row])
-# shuffle our features and turn into np.array
-#random.shuffle(training)
+
 training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
+# creazione dei set di train e di test: X - patterns, Y - intents
 train_x = list(training[:,0])
 train_y = list(training[:,1])
-print("Training data created")
 
-
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# creazione del modello
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
@@ -88,12 +72,11 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 #fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=300, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
-print("model created")
+print("Modello creato!")
